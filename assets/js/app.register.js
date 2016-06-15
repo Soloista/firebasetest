d884ac38026c1e92c983d10b36730748fe28854c
John Eric Orolfo <jorolfo@usautoparts.com>
2016-06-15 16:56:22 +0800

    Initial

---
+++ assets/js/app.register.js
@@ -0,0 +1,20 @@
+// below checks if the user is already signed in
+(function () {
+    let user = firebase.auth().currentUser;
+    if (user) {
+        window.location = 'app.html';
+    }
+})();
+
+var regForm = document.querySelector('form#registrationForm');
+regForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let email = document.querySelector('input[name=email]').value;
+    let pass = document.querySelector('input[name=pass]').value;
+    firebase.auth().createUserWithEmailAndPassword(email, pass).then(function (event) {
+        console.log('successful');
+        window.location = 'app.html'
+    }).catch(function (error) {
+        console.log(error);
+    })
+});
---
+++ assets/js/firebase.config.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAy12g3xOuFUJBPqdxHhRb_btoNi-V4UmA",
+    authDomain: "sample-eb2be.firebaseapp.com",
+    databaseURL: "https://sample-eb2be.firebaseio.com",
+    storageBucket: "sample-eb2be.appspot.com",
+};
+firebase.initializeApp(config);
